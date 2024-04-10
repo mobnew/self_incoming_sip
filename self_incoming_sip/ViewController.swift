0414ea6bbf8a0442d51613ececce3bea62a1cdea
Aleksei Kurazhov <mob_new@mail.ru>
2024-04-10 14:32:08 +0300

    Initial Commit

---
+++ self_incoming_sip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  self_incoming_sip
+//
+//  Created by Alexey Kurazhov on 10.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
